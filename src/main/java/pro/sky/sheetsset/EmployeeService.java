4d90c6710afeb7fd2f7856c87b2d6e570881f187
vaceslavkameronov <slka@mail.ru>
2023-04-03 22:42:57 +0400

    создание проекта ДЗ по теме: Листы и Сеты.

---
+++ src/main/java/pro/sky/sheetsset/EmployeeService.java
@@ -0,0 +1,5 @@
+package pro.sky.sheetsset;
+
+public class EmployeeService {
+    
+}
---
+++ src/main/java/pro/sky/sheetsset/SheetsSetApplication.java
@@ -0,0 +1,14 @@
+package pro.sky.sheetsset;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SheetsSetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SheetsSetApplication.class, args);
+
+    }
+
+}
