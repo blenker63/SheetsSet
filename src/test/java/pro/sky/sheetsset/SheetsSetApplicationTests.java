6c2aaa3bbd1ff99e6e9e8d8e83bb22882bf10f34
vaceslavkameronov <slka@mail.ru>
2023-04-03 22:46:14 +0400

    выполнение ДЗ

---
+++ src/test/java/pro/sky/sheetsset/SheetsSetApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.sheetsset;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SheetsSetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
