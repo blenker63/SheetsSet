3f6298de1722238eb58f7817409a8831ef79a629
vaceslavkameronov <slka@mail.ru>
2023-04-06 21:16:36 +0400

    выполнение ДЗ

--- src/main/java/pro/sky/sheetsset/Employee.java
+++ src/main/java/pro/sky/sheetsset/Employee.java
@@ -7,7 +7,7 @@ public class Employee {
     private String firstName;
     private String lastName;
 
-    public Employee(String firstName, String lastName) {
+    public void employee(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
 
--- src/main/java/pro/sky/sheetsset/EmployeeService.java
+++ src/main/java/pro/sky/sheetsset/EmployeeService.java
@@ -1,5 +1,34 @@
 package pro.sky.sheetsset;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class EmployeeService {
-    
+
+    private Employee[] employees;
+    private static int counter = 0;
+    public EmployeeService() {
+        this.employees = new Employee[10]; // объявление длины массива
+    }
+
+    public void addEmployee(String firstName, String lastName) {
+        if (counter >= employees.length) {
+            System.out.println("Нельзя добавить сотрудника, закончилось место");
+        }
+        Employee newEmployee = new Employee();
+        employees[counter++] = newEmployee;
+    }
+
+    public void removeEmployee(String firstName, String lastName) {
+        for (int i = 0; i < employees.length; i++) {
+            if (employees[i].getFirstName().equals(firstName) && employees[i].getLastName().equals(lastName)) {
+                System.out.println(employees[i].getFirstName() + " " + employees[i].getLastName() + " удален");
+                System.arraycopy(employees, i + 1, employees, i, counter - i - 1);
+                counter-;
+                return;
+            }
+        }
+    }
+
+
 }
